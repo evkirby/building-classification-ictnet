74a33d099d5791a12b82ff843669ecd01db891db
Evan Kirby McGregor <evan.kirby-mcgregor@polymtl.ca>
2021-04-15 19:35:02 -0400

    tf -> keras se block

--- ICTNet/squeeze_excitation_block.py
+++ ICTNet/squeeze_excitation_block.py
@@ -1,18 +1,30 @@
 import numpy as np
-import tensorflow as tf
-import tensorflow.contrib.slim as slim
+from tensorflow import keras
+from keras import layers
+from keras import models
 
-def squeeze_excitation_layer(inputs, output_dimension, ratio, layer_name):
-    with tf.name_scope(layer_name):
-        squeeze = tf.reduce_mean(inputs, axis=[1, 2])
+# Tenserflow
+# def squeeze_excitation_layer(inputs, output_dimension, ratio, layer_name):
+#     with tf.name_scope(layer_name):
+#         squeeze = tf.reduce_mean(inputs, axis=[1, 2])
 
-        units = output_dimension/ratio
-        excitation = tf.layers.dense(inputs=squeeze, use_bias=True, units=units, layer_name=layer_name+'_fully_connected_1')
-        excitation = tf.nn.relu(excitation)
-        excitation = tf.layers.dense(inputs=excitation, use_bias=True, units=output_dimension, layer_name=layer_name+'_fully_connected_2')
-        excitation = tf.nn.sigmoid(excitation)
+#         units = output_dimension/ratio
+#         excitation = tf.layers.dense(inputs=squeeze, use_bias=True, units=units, layer_name=layer_name+'_fully_connected_1')
+#         excitation = tf.nn.relu(excitation)
+#         excitation = tf.layers.dense(inputs=excitation, use_bias=True, units=output_dimension, layer_name=layer_name+'_fully_connected_2')
+#         excitation = tf.nn.sigmoid(excitation)
 
-        excitation = reshape(excitation, [-1, 1, 1, output_dimension])
-        return inputs * excitation
+#         excitation = reshape(excitation, [-1, 1, 1, output_dimension])
+#         return inputs * excitation
 
+# Keras
+def squeeze_excitation_layer(inputs, output_dimension, ratio):
+    squeeze = np.mean(inputs, axis=[1, 2])
+
+    units = output_dimension/ratio    
+    excitation = layers.Dense(units, activation='relu')(squeeze)
+    excitation = layers.Dense(output_dimension, activation='sigmoid')(excitation)
+
+    excitation = excitation.reshape(-1, 1, 1, output_dimension)
+    return inputs * excitation
 

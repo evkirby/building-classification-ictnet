dd81a7efda780498c388804aaf5912aad85dde8e
Jonathan <jonathan.helias@polymtl.ca>
2021-04-17 12:08:36 -0400

    working model

--- ICTNet/model.py
+++ ICTNet/model.py
@@ -8,9 +8,10 @@ from squeeze_excitation_block import squeeze_excitation_layer
 # ------------- #
 
 # -- Variables -- #
-IMG_SIZE = 512
-N_CHANNELS = 1
+IMG_SIZE = 256
+N_CHANNELS = 3
 N_CLASSES = 2 # binary
+BATCH_SIZE = 4
 eps=1e-5
 compress_factor = 0.5
 # --------------- #
@@ -19,8 +20,8 @@ compress_factor = 0.5
 def pre_activation_conv(inputs, n_filters, filter_size=(3, 3), dropout_rate=0.2):
     x = BatchNormalization(epsilon=eps)(inputs)
     x = ReLU()(x)
-    x = ZeroPadding2D((1, 1))(x)
-    x = Conv2D(n_filters, kernel_size=filter_size, use_bias=False, kernel_initializer='he_normal')(x)
+    # x = ZeroPadding2D((1, 1))(x)
+    x = Conv2D(n_filters, kernel_size=filter_size, padding='same', use_bias=False, kernel_initializer='he_normal')(x)
     x = Dropout(rate=dropout_rate)(x)
     return x
 
@@ -43,32 +44,18 @@ def dense_block(inputs, num_layers, growth_rate, dropout_rate, path_type, out_di
 
 
 def TransitionDown(inputs, n_filters, dropout_rate):
-    # compression_factor is the 'θ'
-    # x = BatchNormalization(epsilon=eps)(inputs)
-    # x = Activation('relu')(x)
-    # num_feature_maps = inputs.shape[1] # The value of 'm'
-
-    # x = Conv2D(np.floor(compression_factor * num_feature_maps).astype(np.int),
-    #            kernel_size=(1, 1),
-    #            use_bias=False,
-    #            padding='same',
-    #            kernel_initializer='he_normal',
-    #            kernel_regularizer=keras.regularizers.l2(1e-4)
-    #            )(x)
-    # x = Dropout(rate=dropout_rate)(x)
     x = pre_activation_conv(inputs, n_filters, (1, 1), dropout_rate)
     x = MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(x)  # Should be max pooling ?
     return x
 
 
 def TransitionUp(inputs, n_filters, skip_connection):
-    # skip_connection = ZeroPadding2D((1, 1))(skip_connection)
-    x = Conv2DTranspose(n_filters, kernel_size=(3, 3), strides=(2, 2))(inputs)
+    x = Conv2DTranspose(n_filters, kernel_size=(3, 3), padding='same', strides=(2, 2))(inputs)
     x = Concatenate()([x, skip_connection])
     return x
 
 
-def build_model(preset_model='FC-DenseNet56', num_classes=2, n_filters_first_conv=48, n_pool=5, growth_rate=12, n_layers_per_block=4, dropout_rate=0.2):
+def build_model(preset_model='FC-DenseNet103', num_classes=2, n_filters_first_conv=48, n_pool=5, growth_rate=12, n_layers_per_block=4, dropout_rate=0.2):
     if preset_model == 'FC-DenseNet56':
         n_pool = 5
         growth_rate = 12
@@ -86,10 +73,9 @@ def build_model(preset_model='FC-DenseNet56', num_classes=2, n_filters_first_con
         n_layers_per_block = [n_layers_per_block] * (2 * n_pool + 1)
 
     input_shape = (IMG_SIZE, IMG_SIZE, N_CHANNELS)
-
     # Downsampling path
     inputs = Input(shape=input_shape)
-    x = Conv2D(n_filters_first_conv, kernel_size=(3, 3), use_bias=False, kernel_initializer='he_normal',
+    x = Conv2D(n_filters_first_conv, kernel_size=(3, 3), padding='same', use_bias=False, kernel_initializer='he_normal',
                kernel_regularizer=keras.regularizers.l2(1e-4))(inputs)
 
     n_filters = n_filters_first_conv
@@ -128,14 +114,14 @@ def build_model(preset_model='FC-DenseNet56', num_classes=2, n_filters_first_con
     # x = GlobalAveragePooling2D()(x)
     # x = Dense(N_CLASSES)(x)  # Num Classes
     # outputs = Activation('softmax')(x)
-    net = Conv2D(num_classes, kernel_size=(1, 1))(x)
-    prediction = np.argmax(net, axis=3).astype(int)
-    probas = Softmax()(prediction)
+    net = Conv2D(num_classes, padding='same', kernel_size=(1, 1))(x)
+    prediction = keras.backend.argmax(net, axis=3)
+    probas = Softmax()(net)
 
     model = keras.models.Model(inputs, probas)
     model.compile(loss=keras.losses.categorical_crossentropy,
-                  optimizer=keras.optimizers.RMSprop(learning_rate=0.0001, decay=0.995), #keras.optimizers.SGD(lr=0.001),
-                  metrics=['acc'])
+                  optimizer=keras.optimizers.RMSprop(learning_rate=0.0001, decay=0.995),
+                  metrics=['loss'])
     model.summary()
 # --------------- #
 
